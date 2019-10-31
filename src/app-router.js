7e2c71561dc6d51d56e2aa9c5a5a86ebf9cb505b
cdvines@iu.edu <cdvines623@gmail.com>
2019-10-31 13:13:24 -0400

    comingsoon is the first screen

---
+++ src/app-router.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import ComingSoon from './pages/comingsoon';
+import Home from './pages/home';
+import './app-router.css';
+
+function App() {
+  return (
+    <Router>
+
+
+      <Route path="/" exact component={ComingSoon} />
+      <Route path="/home" component={Home} />
+
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/comingsoon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../css/comingsoon.css";
+
+function ComingSoon() {
+    return (
+        <div className="full vertical-align">
+            <h1>Coming Soon</h1>
+        </div>
+    )
+}
+
+export default ComingSoon;
