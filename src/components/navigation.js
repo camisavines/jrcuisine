d303125ee1f18d56b2b782356325026571de5139
cdvines@iu.edu <cdvines623@gmail.com>
2019-11-03 00:46:35 -0400

    commit testing

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,58 +5,25 @@ import '../css/navigation.css';
 function Navigation() {
     return (
         <nav class="mb-1 navbar navbar-expand-lg navbar-dark">
-            <a class="navbar-brand" href="#">J.R. Cuisine</a>
+            <Link to="/home" className="navbar-brand">J.R. Cuisine</Link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
                 aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
                 <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home
-                    <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false">Dropdown
-                    </a>
-                    <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                </ul>
-                <ul class="navbar-nav ml-auto nav-flex-icons">
-                <li class="nav-item">
-                    <a class="nav-link waves-effect waves-light">
-                    <i class="fab fa-twitter"></i>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link waves-effect waves-light">
-                    <i class="fab fa-google-plus-g"></i>
-                    </a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-user"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right dropdown-default"
-                    aria-labelledby="navbarDropdownMenuLink-333">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
+                    <li class="nav-item active">
+                        <Link to="/home" className="nav-link">Page 1</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/home" className="nav-link">Page 1</Link>
+                        {/* <a class="nav-link" href="#">Features</a> */}
+                    </li>
+                    <li class="nav-item">
+                    <Link to="/home" className="nav-link">Page 1</Link>
+                        {/* <a class="nav-link" href="#">Pricing</a> */}
+                    </li>
                 </ul>
             </div>
         </nav>
