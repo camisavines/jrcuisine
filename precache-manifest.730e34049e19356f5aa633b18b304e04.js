00b261e04fa7fcf6ffa7844a39f05c64a044681a
cdvines@iu.edu <cdvines623@gmail.com>
2019-11-03 00:08:20 -0400

    Updates

---
+++ precache-manifest.730e34049e19356f5aa633b18b304e04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0942ec23db52ba860ad89a3d358e9d66",
+    "url": "/jrcuisine/index.html"
+  },
+  {
+    "revision": "738e33f43574c65655c1",
+    "url": "/jrcuisine/static/css/main.0562b5a7.chunk.css"
+  },
+  {
+    "revision": "3f2644947ae6c9e6d18f",
+    "url": "/jrcuisine/static/js/2.21286a27.chunk.js"
+  },
+  {
+    "revision": "738e33f43574c65655c1",
+    "url": "/jrcuisine/static/js/main.167e0d23.chunk.js"
+  },
+  {
+    "revision": "a9a18bc19cb3c15700e3",
+    "url": "/jrcuisine/static/js/runtime-main.a803adac.js"
+  },
+  {
+    "revision": "a70749148cde51e485fdca7de6fbe566",
+    "url": "/jrcuisine/static/media/IMG_0020.a7074914.JPG"
+  },
+  {
+    "revision": "bb3ba7d0b44c7c9dab644f97449d6031",
+    "url": "/jrcuisine/static/media/instagram-white.bb3ba7d0.png"
+  },
+  {
+    "revision": "917cd80df4ff0c56ee2e60635b42b854",
+    "url": "/jrcuisine/static/media/logo.917cd80d.jpeg"
+  }
+]);
