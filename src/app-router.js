bf3c713eb0b063ebb5605691b31c5b51d03f804f
cdvines@iu.edu <cdvines623@gmail.com>
2019-11-04 13:03:33 -0500

    updates 11-4-2019

--- src/app-router.js
+++ src/app-router.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
 // import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import ComingSoon from './pages/comingsoon';
 import Home from './pages/home';
-import './app-router.css';
+import './css/index.css';
 
 function App() {
   return (
     <Router>
 
-        <Route path="/comingsoon" exact component={ComingSoon} />
+        <Route path="/" exact component={ComingSoon} />
         <Route path="/home" component={Home} />
 
     </Router>
